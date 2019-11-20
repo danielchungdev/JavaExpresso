08307f63b21f0397924266e9d43b87bed9c5f39e
pochronjacob <52175016+pochronjacob@users.noreply.github.com>
2019-11-20 13:03:37 -0500

    created header and changed lesson to php format

---
+++ assets/inc/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--[start the html document]-->
+<html lang="en">
+<head>
+	<title><?php echo $page;?></title>
+	<link rel="stylesheet" type="text/css" href="<?php echo $path;?>assets/css/pizza.css"/>
+	<script src="<?php echo $path;?>assets/script/pizza.js"></script>
+</head>
+
+<body>
+	<div id="wrapper">
+		<h1>Corleone Pizzeria</h1>
+
+		<div id="featureimage">
+			<img alt="Image of a pizza pie" src="<?php echo $path;?>assets/img/pizza.jpg">
+		</div>
