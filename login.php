31dd7d137c387c7d5b2cfc901b8f0fe6ff6ea270
johntwarner <57264984+johntwarner@users.noreply.github.com>
2019-12-01 22:22:45 -0500

    Finally got them done
    just check the db connects to make sure they work with everything

---
+++ login.php
@@ -0,0 +1,64 @@
+<?php
+    session_start();
+
+    $path = './';
+    require $path.'../../dbConnect.inc';
+    
+
+    if(!empty($_POST['email']) && !empty($_POST['psw']) ){
+        $email=$_POST['email'];
+         
+        $pass=$_POST['psw'];
+        
+        $stmt=$mysqli->prepare("SELECT password FROM accountDb WHERE userEmail=?");
+        $stmt->bind_param("s",$email);
+        $stmt->execute();
+        $stmt->bind_result($res);
+		$stmt->fetch();
+        if (password_verify($_POST['psw'], $res)) {
+            $_SESSION['login']=true;
+            $_SESSION['email']=$email;
+                session_name($_POST['email'].'s');
+			header('Location: survey.php');
+        }
+        else{
+            
+                
+        }
+       $stmt->close();
+    }
+
+    session_destroy(); 
+         
+     
+?>
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Login</title>
+        <link rel="stylesheet" href="asset/css/styles.css">
+        <script src="asset/js/formVal.js"></script>
+    </head>
+    <body>
+         <div class="container">
+            <h2>Login</h2>
+            <p>Please fill in this form to login</p>
+
+            <form name="logForm" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" onsubmit="return logVal()" >
+                <label for="email"><b>Email</b></label>
+                <input type="text" placeholder="Enter Email" name="email" />
+                <br>
+                <br>
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw"/>
+                <br>
+                <br>
+                <input type="submit" name="Submit"/>
+            </form>
+        </div>
+        
+    </body>
+</html>
--- registration.php
+++ registration.php
@@ -2,23 +2,24 @@
     $path = './';
     require $path.'../../dbConnect.inc';
 
-    if(!empty($_POST['email']) && !empty($_POST['psw']) && !empty($_POST['pswR']) && passCheck() && eCheck()){
+    if(!empty($_POST['r_email']) && !empty($_POST['r_psw']) && !empty($_POST['r_pswR']) && passCheck() && eCheck()){
                   
-        $email=$_POST['email'];
+        $email=$_POST['r_email'];
          
-        $pass=$_POST['psw'];
+        $pass=$_POST['r_psw'];
       try{
-        $pass=password_hash($_POST['psw'], PASSWORD_DEFAULT);
+        
         $stmt=$mysqli->prepare("INSERT INTO accountDb (userEmail, password) VALUES(?,?)");
-        $stmt->bind_param("ss",$email, $pass);
+        $stmt->bind_param("ss",$email, password_hash($_POST['r_psw'], PASSWORD_DEFAULT));
 		$stmt->execute();
 		$stmt->close();
       }catch(Exception $e){
           if($mysqli->errno === 1062) echo 'Duplicate entry';
       }
+        header('Location: login.php');
     }
     function passCheck(){
-		if(strcmp($_POST['psw'],$_POST['pswR'])==0){
+		if(strcmp($_POST['r_psw'],$_POST['r_pswR'])==0){
 			return true;
 		}else{
 			return false;
@@ -26,7 +27,7 @@
 	}    
     function eCheck(){
         
-        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        if (!filter_var($_POST['r_email'], FILTER_VALIDATE_EMAIL)) {
             return false;
         }
          return true;
@@ -45,7 +46,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Registration</title>
         <link rel="stylesheet" href="asset/css/styles.css">
-        <script src="asset/js/regVal.js"></script>
+        <script src="asset/js/formVal.js"></script>
     </head>
     <body>
         
@@ -55,19 +56,19 @@
     <h2>Register</h2>
     <p>Please fill in this form to create an account.</p>
 
-    <form name="regForm" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" onsubmit="return formval()" >
+    <form name="regForm" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" onsubmit="return regVal()" >
 
         <label for="email"><b>Email</b></label>
-        <input type="text" placeholder="Enter Email" name="email" />
+        <input type="text" placeholder="Enter Email" name="r_email" />
         <br>
         <br>
         <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw"/>
+        <input type="password" placeholder="Enter Password" name="r_psw"/>
     
         <br>
         <br>
         <label for="psw-repeat"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Repeat Password" name="pswR" />
+        <input type="password" placeholder="Repeat Password" name="r_pswR" />
  
 
         <br>
@@ -80,7 +81,7 @@
 
      </form>
     </div>
-    
+     
 </body>
         
 </html>
