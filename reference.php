326228419a35c46bc1de61249b92c01c850430e4
Jacob Pochron <pochron.jacob@gmail.com>
2019-12-02 21:19:17 -0500

    references

--- reference.php
+++ reference.php
@@ -5,13 +5,12 @@
 	include $path.'assets/inc/header.php';
 	require $path.'../../../dbConnect.inc';
 ?>
-
-            <div class="flex-container">
+            <div class="flex-container" style="width:70%">
                 <div class="inner-element">
-                    <h1 >REFERENCES</h1>
+                    <h2>REFERENCES</h2>
                 </div>
             </div>
-            <div class="flex-container-table">
+            <div class="flex-container-table" style="width:70%">
                 <table>
                     <tr>
                         <th>URL</th>
@@ -19,12 +18,12 @@
                         <th>Use Where?</th>
                     </tr>
                     <tr>
-                        <td>https://www.w3schools.com/howto/howto_js_slideshow.asp</td>
+                        <td><a href="https://www.w3schools.com/howto/howto_js_slideshow.asp">Slideshow</a></td>
                         <td>Nov 13th, 2019</td>
                         <td>index.html</td>
                     </tr>
                     <tr>
-                        <td>https://www.w3docs.com/snippets/html/how-to-create-an-html-button-that-acts-like-a-link.html</td>
+                        <td><a href="https://www.w3docs.com/snippets/html/how-to-create-an-html-button-that-acts-like-a-link.html">HTMl Buttons</a></td>
                         <td>Nov 30th, 2019</td>
                         <td>index.html, login.php, registration.php</td>
                     </tr>
