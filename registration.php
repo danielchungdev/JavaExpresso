8ec0d5e883ac912339cb3513a7639c6843979557
Jacob Pochron <pochron.jacob@gmail.com>
2019-12-02 19:47:13 -0500

    add style to registration php

--- registration.php
+++ registration.php
@@ -50,38 +50,39 @@
         <script src="assets/js/formVal.js"></script>
     </head>
     <body>
+      <div class="loginAndRegistration">
+          <div id="logo_image">
+            <img src="assets/images/feature_logo.png"/>
+          </div>
+          <div class="container">
+              <h2>Register</h2>
+
+              <form name="regForm" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" onsubmit="return regVal()" >
+
+                  <label for="email"><b>Email</b></label><br />
+                  <input type="text" placeholder="email" name="r_email" />
+                  <br>
+                  <br>
+                  <label for="psw"><b>Password</b></label><br />
+                  <input type="password" placeholder="password" name="r_psw"/>
+
+                  <br>
+                  <br>
+                  <label for="psw-repeat"><b>Repeat Password</b></label><br />
+                  <input type="password" placeholder="repeat password" name="r_pswR" />
 
 
+                  <br>
+                  <br>
+                  <input type="submit" value="Submit"/>
 
- <div class="container">
-    <h2>Register</h2>
-    <p>Please fill in this form to create an account.</p>
+                  <br>
+                  <br>
+                  <p style="text-align: center;">Already have an account? <a style="color:#F5EDDA; " href="login.php">Sign in</a>.</p>
 
-    <form name="regForm" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" onsubmit="return regVal()" >
-
-        <label for="email"><b>Email</b></label>
-        <input type="text" placeholder="Enter Email" name="r_email" />
-        <br>
-        <br>
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="r_psw"/>
-
-        <br>
-        <br>
-        <label for="psw-repeat"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Repeat Password" name="r_pswR" />
-
-
-        <br>
-        <br>
-        <input type="submit" value="Submit"/>
-
-        <br>
-        <br>
-        <p>Already have an account? <a style="color:#F5EDDA; " href="login.php">Sign in</a>.</p>
-
-     </form>
-    </div>
+               </form>
+            </div>
+          </div>
 
 </body>
 
